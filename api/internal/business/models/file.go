f8d4676f559dbf0d64da834416f2bad98548867c
MistDev <developerofshadows@gmail.com>
2024-10-20 21:29:23 +0300

    Add document struct

--- api/internal/business/models/file.go
+++ api/internal/business/models/file.go
@@ -1,4 +1,20 @@
 package models
 
+import "time"
+
 type Document struct {
+	FIO               string    `json:"fio,omitempty"`
+	University        string    `json:"university,omitempty"`
+	StudyForm         string    `json:"study_form,omitempty"`
+	Course            string    `json:"course,omitempty"`
+	Group             string    `json:"group,omitempty"`
+	Specialty         string    `json:"specialty,omitempty"`
+	Profile           string    `json:"profile,omitempty"`
+	SnilsId           string    `json:"snils_id,omitempty"`
+	PassportId        string    `json:"passport_id,omitempty"`
+	PassportIssueDate time.Time `json:"passport_issue_date,omitempty"`
+	Birthdate         time.Time `json:"birthdate,omitempty"`
+	Email             string    `json:"email,omitempty"`
+	Phone             string    `json:"phone,omitempty"`
+	TelegramId        string    `json:"telegram_id"`
 }
