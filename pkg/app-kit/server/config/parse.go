c54dae90222f9c1c7055517c4fbc74c474ca7468
MistDev <developerofshadows@gmail.com>
2024-11-08 05:29:20 +0300

    Add Viper support maybe now it works with env vars

--- pkg/app-kit/server/config/parse.go
+++ pkg/app-kit/server/config/parse.go
@@ -1,19 +1,23 @@
 package config
 
 import (
-	"gopkg.in/yaml.v3"
-	"os"
+	"github.com/spf13/viper"
 )
 
 func Parse[C any]() *C {
 	cfg := new(C)
 
-	f, err := os.ReadFile("config/config.yaml")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
 	if err != nil {
 		panic(err)
 	}
 
-	err = yaml.Unmarshal(f, &cfg)
+	err = viper.Unmarshal(cfg)
 
 	return cfg
 }
